501706036ae3f8c7347abb387beb4e81388414b1
komiya346 <1mg9720s@komazawa-u.ac.jp>
2024-06-09 10:43:39 +0900

    Initial Commit

---
+++ practice1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  practice1
+//
+//  Created by 越智三四郎 on 2024/06/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ practice1/practice1App.swift
@@ -0,0 +1,17 @@
+//
+//  practice1App.swift
+//  practice1
+//
+//  Created by 越智三四郎 on 2024/06/09.
+//
+
+import SwiftUI
+
+@main
+struct practice1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
